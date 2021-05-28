fcda4e1839b416975d3885b541a93884e1528579
77web <info@77-web.com>
2021-05-28 12:26:04 +0900

    removed swiftmailer-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
